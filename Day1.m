3f79cdca6d8835d0ffd123e8f1bded28f22c01bd
Colton Forry <colton4e@gmail.com>
2021-12-01 20:47:49 -0500

    Day 2 part 2

--- Day1.m
+++ Day1.m
@@ -1,8 +1,21 @@
+%% Part 1
 clc,clear
 in = readmatrix('input.txt')';
 summ = 0;
 for i = 1:length(in)-1
     if in(i+1)>in(i)
+        summ = summ + 1;
+    end
+end
+fprintf('The answer is %0.f\n', summ)
+%% Part 2
+clc,clear
+in = readmatrix('input.txt')';
+summ = 0;
+for i = 1:length(in)-3
+    sumA = in(i)+in(i+1)+in(i+2);
+    sumB = in(i+1)+in(i+2)+in(i+3);
+    if sumB>sumA
         summ = summ + 1;
     end
 end
